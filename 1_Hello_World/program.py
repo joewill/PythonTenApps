b8489a247aad7c65d137712303991fefb968d52f
Joe Williams <joewill@gmail.com>
2017-08-12 20:59:12 -0400

    first app, hello world, classic

---
+++ 1_Hello_World/program.py
@@ -0,0 +1,10 @@
+print('---------------------------------------------')
+print('                 HELLO WORLD')
+print('---------------------------------------------')
+print() #Leaves a 
+
+#in Python we use the underscore for multi-word variables
+user_text = input('What is your name? ') 
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
