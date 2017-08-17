ac83e03061069fe4574cb10da8bd50439e0b9647
Joe Williams <joewill@gmail.com>
2017-08-17 19:29:41 -0400

    saving the journal to a file

--- 4_journal/journal.py
+++ 4_journal/journal.py
@@ -1,11 +1,30 @@
+import os
+
 
 def load(name):
-    return []
+    data = []
+    filename = get_full_pathname(name)
 
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', '4_journal', 'journals', name + '.jrl'))
+    return filename
 
 def save(name, journal_data):
-    pass
+    filename = get_full_pathname(name)
+    print("....saving to: {}".format(filename))
+
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
 
 
-def add_entry(text, journal_data)
+def add_entry(text, journal_data):
     journal_data.append(text)
