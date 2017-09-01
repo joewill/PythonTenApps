73428a41c09372c1bf928b8e862ca6924e7fb0a0
Joe Williams <joewill@gmail.com>
2017-08-31 21:09:04 -0400

    LOL Cat App

--- 3_birthday/program.py
+++ 3_birthday/program.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def print_header():
     print('---------------------------------------------')
     print('                 HELLO WORLD')
@@ -17,7 +18,6 @@ def get_birthday_from_user():
     return birthday
 
 
-
 def compute_days_between_dates(original_date, now):
     date1 = now
     date2 = datetime.datetime(now.year, original_date.month, original_date.day)
@@ -30,10 +30,17 @@ def print_birthday_info(number_of_days):
     if number_of_days < 0:
         print('Your birthday is in {} days!'.format(-number_of_days))
     elif number_of_days > 0:
-        print('You had your birthday already this year! {} days ago'.format(number_of_days))
+        print('You had your birthday already this year! {} days ago'.format(
+            number_of_days))
     else:
         print("Happy Birthday!")
 
+
+def indentation_example():
+    result = call_to_other_function()
+
+    if result == 'Cool'
+        return True
 
 
 def main():
