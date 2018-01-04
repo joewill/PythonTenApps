7013ebbe7d551e085cfd96757cc3a98194a0aecd
Joe Williams <joewill@gmail.com>
2018-01-04 10:01:33 -0700

    added error handling

--- 10_movie_search/program.py
+++ 10_movie_search/program.py
@@ -1,4 +1,5 @@
 import movie_svc
+import requests.exceptions
 
 
 def main():
@@ -17,13 +18,20 @@ def search_event_loop():
     search = 'ONCE_THROUGH_LOOP'
 
     while search != 'x':
-        search = input('Movie search text (x to exit): ')
-        if search != 'x':
-            results = movie_svc.find_movies(search)
-            print("Found {} results".format(len(results)))
-            for r in results:
-                print('{} -- {}'.format(r.year, r.title))
-            print()
+        try:
+            search = input('Movie search text (x to exit): ')
+            if search != 'x':
+                results = movie_svc.find_movies(search)
+                print("Found {} results".format(len(results)))
+                for r in results:
+                    print('{} -- {}'.format(r.year, r.title))
+                print()
+        except ValueError:
+            print("Error: Search text is required.")
+        except requests.exceptions.ConnectionError:
+            print("Error: Could not find a network connection.")
+        except Exception as x:
+            print("Unexpected Error! Details: {}".format(x))
 
     print('exiting...')
 
